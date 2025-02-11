c7a09753dafecaef5974b1c881e7b685bab2ee7b
FakeColdMilk <aaron.nylund2@gmail.com>
2025-02-11 12:36:48 +0200

    works but ugly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+const random = Math.floor(Math.random() * 100) + 1;
 
 export default function Home() {
+const [number, setNumber] = useState(10);
+const decrase = () => {
+  setNumber(number - 1);
+};
+const increase = () => {
+    setNumber(number + 1);
+};
+function guess() {
+  if (number < random) {
+  alert("Too low");
+} else if (number > random) {
+  alert("Too high");
+} else
+if (number == random) {
+  alert("You guessed right");
+}
+}
+
   return (
     
     <div className="bg-blue-500">
-      <h1>hej</h1>
+      {
+        number
+      }
       
+      <button onClick={guess}>click2</button>
+     
+     <input type="text" onChange={(e)=>setNumber (e.target.value)} 
+     
+     />
     </div>
   );
 }
